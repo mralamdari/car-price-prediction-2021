9c49bcc57ea1474872df966fe6a10e457d999fd7
rwrdvsqaesdfh32s <91946558+rwrdvsqaesdfh32s@users.noreply.github.com>
2021-10-21 22:47:36 +0330

    Update app.py

--- app.py
+++ app.py
@@ -23,28 +23,31 @@ def help():
 
 @app.route("/about")
 def about():
-    white_car = os.path.join(app.config['UPLOAD_FOLDER'], 'white_car.jpg')
-    return flask.render_template("about.html", white=white_car)
+    silver_car = os.path.join(app.config['UPLOAD_FOLDER'], 'silver_car.jpg')
+    return flask.render_template("about.html", silver=silver_car)
 
 @app.route("/results", methods=["GET", "POST"])
 def results():
-    silver_car = os.path.join(app.config['UPLOAD_FOLDER'], 'silver_car.jpg')
-    carwidth = float(flask.request.args.get("carwidth"))
-    curbweight = float(flask.request.args.get("curbweight"))
-    enginesize = float(flask.request.args.get("enginesize"))
-    compressionratio = float(flask.request.args.get("compressionratio"))
-    horsepower = float(flask.request.args.get("horsepower"))
-    highwaympg = float(flask.request.args.get("highwaympg"))
-    citympg = float(flask.request.args.get("citympg"))    
-    fueleconomy=(highwaympg + citympg)//2
+    white_car = os.path.join(app.config['UPLOAD_FOLDER'], 'white_car.jpg')
+    try:
+        carwidth = float(flask.request.args.get("carwidth"))
+        curbweight = float(flask.request.args.get("curbweight"))
+        enginesize = float(flask.request.args.get("enginesize"))
+        compressionratio = float(flask.request.args.get("compressionratio"))
+        horsepower = float(flask.request.args.get("horsepower"))
+        highwaympg = float(flask.request.args.get("highwaympg"))
+        citympg = float(flask.request.args.get("citympg"))    
+        fueleconomy=(highwaympg + citympg)//2
 
-    input_list = np.array([carwidth, curbweight, enginesize, compressionratio,horsepower, highwaympg, fueleconomy, citympg])
-    loadm = pickle.load(open("h2_all_best_model.sav", "rb"))
-    le = preprocessing.MinMaxScaler()
-    test = pd.DataFrame(le.fit_transform(input_list.reshape(-1, 1))).T
-    pred=loadm.predict(test)
+        input_list = np.array([carwidth, curbweight, enginesize, compressionratio,horsepower, highwaympg, fueleconomy, citympg])
+        loadm = pickle.load(open("h2_all_best_model.sav", "rb"))
+        le = preprocessing.MinMaxScaler()
+        test = pd.DataFrame(le.fit_transform(input_list.reshape(-1, 1))).T
+        pred=loadm.predict(test)
+    except TypeError:
+        return flask.render_template("index.html", white=white_car)
 
-    return flask.render_template("results.html", silver=silver_car, res=round(pred[0], 4))
+    return flask.render_template("results.html", white=white_car, res=round(pred[0], 4))
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=os.environ.get("PORT", 5000))   
